0cd6fe86658ce537f316dfa591d75a8d41602079
jaekyuoh <jaekyuoh@jaekyuohui-MacBookPro.local>
2019-07-22 21:44:47 +0900

    Done with github_finder in naive way

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 class App extends React.Component{
   state = {
     users: [],
+    repos: [],
     user: {},
     loading: false,
     alert: null
@@ -49,6 +50,19 @@ class App extends React.Component{
     this.setState({user: res.data, loading: false});
   }
 
+  // Get users repos
+  getUserRepos = async (username) => {
+    this.setState({loading: true});
+    const res = await axios.get(
+      `https://api.github.com/users/${username}/repos?
+      per_page=5
+      &sort=created:asc&
+      &client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}
+      &client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    )
+    this.setState({repos: res.data, loading: false});
+  }
+
   // Clear users from state
   clearUsers = () => {
     this.setState({users:[], loading: false})
@@ -61,7 +75,7 @@ class App extends React.Component{
   }
 
   render(){
-    const {loading, users, user} = this.state
+    const {loading, users, user, repos} = this.state
     return (
       <Router>
         <div className="App">
@@ -83,7 +97,14 @@ class App extends React.Component{
 
                 <Route exact path='/about' component={About}/>
                 <Route exact path='/user/:login' render={props => (
-                  <User {...props} getUser={this.getUser} user={user} loading={loading}/>
+                  <User 
+                    {...props} 
+                    getUser={this.getUser} 
+                    getUserRepos={this.getUserRepos} 
+                    user={user} 
+                    repos={repos}
+                    loading={loading}
+                  />
                 )} />
             </Switch>
           </div>
